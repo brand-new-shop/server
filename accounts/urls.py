be3bf2f8308fb0bfc8b6d5ba87f37dfae3aa3eb4
Eldos <eldos.baktybekov@gmail.com>
2023-01-01 21:08:30 +0600

    :boom: Change ticket detail endpoint route

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
 
 from accounts.views import user_create_view, UserDetailView, OrdersListCreateView, orders_count_view
-from support.views import SupportTicketListCreateView, SupportTicketRetrieveView
 
 urlpatterns = [
     path('', user_create_view),
     path('telegram-id/<int:telegram_id>/', UserDetailView.as_view()),
     path('telegram-id/<int:telegram_id>/orders/', OrdersListCreateView.as_view()),
     path('telegram-id/<int:telegram_id>/orders/statistics/', orders_count_view),
-    path('telegram-id/<int:telegram_id>/tickets/', SupportTicketListCreateView.as_view()),
-    path('telegram-id/<int:telegram_id>/tickets/<int:ticket_id>/', SupportTicketRetrieveView.as_view()),
 ]
---
+++ support/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from support.views import SupportTicketListCreateView, SupportTicketRetrieveView
+
+urlpatterns = [
+    path('telegram-id/<int:telegram_id>/tickets/', SupportTicketListCreateView.as_view()),
+    path('tickets/<int:ticket_id>/', SupportTicketRetrieveView.as_view()),
+]
