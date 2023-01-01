e3fd3159911261e50400e0bea784865fad4e0aca
Eldos <eldos.baktybekov@gmail.com>
2023-01-01 11:06:13 +0600

    :sparkles: Add filtration by ticket creation time

--- support/admin.py
+++ support/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from rangefilter.filters import DateTimeRangeFilter
 
 from support.models import SupportSubject, SupportTicket
 
@@ -10,12 +11,15 @@ class SupportSubjectAdmin(admin.ModelAdmin):
 
 @admin.register(SupportTicket)
 class SupportRequest(admin.ModelAdmin):
-    list_filter = ('status', 'subject')
-    search_fields = ('issue',)
-    search_help_text = 'Search in request\'s issue text'
+    list_filter = ('status', 'subject', ('created_at', DateTimeRangeFilter))
+    search_fields = ('user__telegram_id', 'user__username')
+    search_help_text = 'Please enter Username or Telegram ID of the User you want to show their tickets (leave empty for all Users)'
     ordering = ('-created_at',)
     readonly_fields = ('user', 'subject', 'issue', 'created_at',)
     list_select_related = ('user', 'subject')
 
     def has_add_permission(self, request):
         return False
+
+    def get_rangefilter_created_at_title(self, request, field_path):
+        return 'Created at'
