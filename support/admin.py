3678022ca036ee5fd2993ae7c1be11f9afa05bb6
Eldos <eldos.baktybekov@gmail.com>
2023-01-04 20:25:02 +0600

    :sparkles: Add replies into admin

--- support/admin.py
+++ support/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from rangefilter.filters import DateTimeRangeFilter
 
-from support.models import SupportTicket
+from support.models import SupportTicket, ReplyToTicket
+
+
+class ReplyToTicketInline(admin.TabularInline):
+    model = ReplyToTicket
 
 
 @admin.register(SupportTicket)
-class SupportRequest(admin.ModelAdmin):
+class SupportTicket(admin.ModelAdmin):
+    inlines = (ReplyToTicketInline,)
     list_filter = ('status', 'subject', ('created_at', DateTimeRangeFilter))
     search_fields = ('user__telegram_id', 'user__username')
     search_help_text = 'Please enter Username or Telegram ID of the User you want to show their tickets (leave empty for all Users)'
@@ -18,3 +23,12 @@ class SupportRequest(admin.ModelAdmin):
 
     def get_rangefilter_created_at_title(self, request, field_path):
         return 'Created at'
+
+
+@admin.register(ReplyToTicket)
+class ReplyToTicketAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'created_at')
+    ordering = ('created_at',)
+    search_fields = ('ticket__id',)
+    search_help_text = 'Search by Ticket ID or Reply ID'
+    readonly_fields = ('issue',)
