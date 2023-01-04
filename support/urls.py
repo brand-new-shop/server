753a49f3a92a6e8ad5adaa18fc7bf9a5b1110501
Eldos <eldos.baktybekov@gmail.com>
2023-01-04 18:54:00 +0600

    :boom: Change tickets list route

--- support/urls.py
+++ support/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from support.views import SupportTicketListCreateView, SupportTicketRetrieveView
 
 urlpatterns = [
-    path('telegram-id/<int:telegram_id>/tickets/', SupportTicketListCreateView.as_view()),
+    path('users/telegram-id/<int:telegram_id>/tickets/', SupportTicketListCreateView.as_view()),
     path('tickets/<int:ticket_id>/', SupportTicketRetrieveView.as_view()),
 ]
