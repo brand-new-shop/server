6505dc9cb22de1e92bff19a0e72761d616bcfe44
Eldos <eldos.baktybekov@gmail.com>
2023-01-05 14:16:29 +0600

    :card_file_box: Changes in models
    * Add extra settings in `Product`.
    * Multiple pictures related to one product

--- products/models.py
+++ products/models.py
@@ -26,21 +26,31 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    picture_url = models.ImageField(null=True, blank=True, upload_to='pictures')
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stocks_count = models.SmallIntegerField(default=0)
+    stocks_count = models.PositiveSmallIntegerField(default=0)
     content = models.TextField()
     type = models.CharField(max_length=64, default='text')
+    min_order_quantity = models.PositiveSmallIntegerField(default=1)
+    max_order_quantity = models.PositiveSmallIntegerField(default=1)
+    max_replacement_time_in_minutes = models.PositiveIntegerField(default=15)
+    are_stocks_displayed = models.BooleanField(default=True)
+    is_hidden = models.BooleanField(default=False)
+    can_be_purchased = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
 
 
+class ProductPicture(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='pictures')
+
+    def __str__(self):
+        return f'#{self.id}'
+
+
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    total_price = models.DecimalField(max_digits=12, decimal_places=2)
-    quantity = models.SmallIntegerField()
     payment_type = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
