f4be65d6e761287eb58cbb32c40fe2172a21adf7
Eldos <eldos.baktybekov@gmail.com>
2023-01-05 14:29:44 +0600

    :construction: Change orders logic
    * Separate `order` and `order product` entities

--- products/models.py
+++ products/models.py
@@ -55,4 +55,11 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Order #{self.id} | {self.product}'
+        return f'Order #{self.id}'
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    total_price = models.DecimalField(max_digits=12, decimal_places=2)
