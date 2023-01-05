62177ac9a732ee60fb848e0cfdbb71d6d1c55646
Eldos <eldos.baktybekov@gmail.com>
2023-01-05 14:30:03 +0600

    :construction: Add endpoints for statistics by orders

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from accounts.views import user_create_view, UserDetailView, OrdersListCreateView, orders_count_view
+from accounts.views import (
+    user_create_view,
+    UserDetailView,
+    OrdersListCreateView,
+    orders_statistics_view,
+    orders_count_view,
+)
 
 urlpatterns = [
     path('', user_create_view),
     path('telegram-id/<int:telegram_id>/', UserDetailView.as_view()),
     path('telegram-id/<int:telegram_id>/orders/', OrdersListCreateView.as_view()),
-    path('telegram-id/<int:telegram_id>/orders/statistics/', orders_count_view),
+    path('telegram-id/<int:telegram_id>/orders/statistics/', orders_statistics_view),
+    path('telegram-id/<int:telegram_id>/orders/count/', orders_count_view),
 ]
