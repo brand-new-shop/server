7433d45e123785082dc40161eb23ba297d48a80b
Eldos <eldos.baktybekov@gmail.com>
2023-01-22 15:10:34 +0600

    :technologist: WIP on exceptions
    * Change `TicketIsClosed` error status code from `400` to `409`.
    * Add error detail to `SupportTicketCreationRateLimitExceeded` error

--- support/exceptions.py
+++ support/exceptions.py
@@ -3,12 +3,13 @@ from rest_framework.exceptions import APIException
 
 
 class TicketIsClosed(APIException):
-    status_code = status.HTTP_400_BAD_REQUEST
+    status_code = status.HTTP_409_CONFLICT
     default_detail = 'Ticket was closed, so now only read-only operations available'
 
 
 class SupportTicketCreationRateLimitExceeded(APIException):
     status_code = status.HTTP_429_TOO_MANY_REQUESTS
+    default_detail = 'Ticket creation rate limit exceeded'
 
     def __init__(self, seconds_to_wait: int):
         super().__init__({'seconds_to_wait': seconds_to_wait})
