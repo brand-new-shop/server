b3474223e1ee88ef91f2aac79accd2b8298f6e8f
Eldos <eldos.baktybekov@gmail.com>
2023-05-01 12:25:34 +0600

    :card_file_box: Add `updated_at` field

--- cart/models.py
+++ cart/models.py
@@ -9,6 +9,7 @@ class CartProduct(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(validators=(MinValueValidator(1),))
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         unique_together = ('user', 'product')
