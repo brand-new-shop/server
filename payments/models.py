3fd0cb2bf8397a380ace4318162a9343a818e99e
Eldos <eldos.baktybekov@gmail.com>
2023-05-04 14:58:47 +0600

    :card_file_box: CoinbasePayment model changes
    - Add `status` field.
    - Make `uuid` field unique.

--- payments/models.py
+++ payments/models.py
@@ -4,9 +4,18 @@ from accounts.models import User
 
 
 class CoinbasePayment(models.Model):
+    class Status(models.IntegerChoices):
+        PENDING = 1
+        SUCCESS = 2
+        FAILED = 3
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    uuid = models.UUIDField()
+    uuid = models.UUIDField(unique=True)
     payment_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.PositiveSmallIntegerField(
+        choices=Status.choices,
+        default=Status.PENDING,
+    )
 
     def __str__(self):
         return f'Coinbase: {str(self.uuid)}'
