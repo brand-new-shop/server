9863d391f7e4680f6501ad6ed91bf5904a994ffd
Eldos <eldos.baktybekov@gmail.com>
2023-05-06 20:26:40 +0600

    :technologist: Add API endpoints profiler

--- shop_bot_server/settings.py
+++ shop_bot_server/settings.py
@@ -112,3 +112,7 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if DEBUG:
+    INSTALLED_APPS.append('silk')
+    MIDDLEWARE.append('silk.middleware.SilkyMiddleware')
